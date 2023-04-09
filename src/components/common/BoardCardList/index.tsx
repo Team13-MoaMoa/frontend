dcbf9034f522ba7f6ee19a7d9b616a201bc987ad
jonghun <minn4072@gmail.com>
2023-04-09 23:34:47 +0900

    feat: 게시글 카드 리스트 컴포넌트 구현

---
+++ src/components/common/BoardCardList/index.tsx
@@ -0,0 +1,20 @@
+import styled from '@emotion/styled';
+import React from 'react';
+import BoardCardItem from '../BoardCardItem';
+
+function BoardCardList({ boardCards }: { boardCards: any }) {
+  return (
+    <Div>
+      {boardCards?.map(() => (
+        <BoardCardItem></BoardCardItem>
+      ))}
+    </Div>
+  );
+}
+
+const Div = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+export default BoardCardList;
