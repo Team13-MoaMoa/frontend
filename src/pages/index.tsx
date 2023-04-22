c226c59a3a984aa595bf63cdb42ba10b073e61b8
jonghun <minn4072@gmail.com>
2023-04-22 18:30:26 +0900

    feat: 메인페이지 필요한 기능 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,119 +1,150 @@
 import BoardCardList from '@/components/common/BoardCardList';
 import Pagination from '@/components/main/pagination';
+import techStackData from '@/constants/techStackData';
 import styled from '@emotion/styled';
-// import '@lottiefiles/lottie-player';
 import Image from 'next/image';
+import { useState } from 'react';
+import { FiSearch } from 'react-icons/fi';
+
+import BannerSlider from '@/components/main/slider';
 
 export default function Home() {
-  const techStackMenu = ['프론트엔드', '백엔드', '기타', '모두 보기'];
-  const techItemList = ['React', 'React', 'React'];
+  const [techStackMenu, setTeckStackMenu] = useState([
+    { name: '프론트엔드', type: 'frontend', checked: false },
+    { name: '백엔드', type: 'backend', checked: false },
+    { name: '기타', type: 'etc', checked: false },
+    { name: '모두보기', type: 'all', checked: true },
+  ]);
+  const [techItemList, setTechItemList] = useState(techStackData);
+
+  const checkTechStack = (id: number) => {
+    const newData = techItemList.map((item) =>
+      item.id === id ? { ...item, checked: !item.checked } : item
+    );
+    setTechItemList(newData);
+  };
+
+  const filterTechStack = (type: string) => {
+    if (type === 'all') setTechItemList(techStackData);
+    else {
+      const filterStack = techStackData.filter((item) => item.type === type);
+      setTechItemList(filterStack);
+    }
+  };
+
+  const checkTechMenu = (type: string) => {
+    const checkMenu = techStackMenu
+      .map((item) => ({
+        ...item,
+        checked: false,
+      }))
+      .map((item) => (item.type === type ? { ...item, checked: true } : item));
+    setTeckStackMenu(checkMenu);
+  };
+
   const boardCards = [{}, {}, {}, {}, {}, {}];
+
   return (
     <Div>
-      <section
-        style={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          backgroundColor: '#E3F3FF',
-          height: '22.5rem',
-          marginBottom: '24px',
-        }}
-      >
-        <div
-          style={{
-            width: '32.5rem',
-            height: '8rem',
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'space-between',
-          }}
-        >
-          <div>
-            스터디와 사이드 프로젝트를 찾는 <br></br>가장 쉬운 방법
-          </div>
-          <div>MOA MOA 에서 찾아보세요🔍</div>
-        </div>
-        <div
-          style={{ position: 'relative', width: '32rem', height: '22.5rem' }}
-        >
-          <Image src="/study.gif" fill alt="gif"></Image>
-        </div>
-      </section>
+      <BannerSlider></BannerSlider>
 
-      <section style={{ maxWidth: '1168px', margin: '0 auto' }}>
-        <section
-          style={{
-            display: 'flex',
-            justifyContent: 'space-between',
-            padding: '0 2rem',
-            marginBottom: '10rem',
-          }}
-        >
+      <MainDiv>
+        <section>
           <div>
-            <div style={{ display: 'flex', marginBottom: '1rem' }}>
-              {techStackMenu.map((title) => (
-                <TechStackMenuDiv>{title}</TechStackMenuDiv>
-              ))}
-            </div>
-            <ul style={{ display: 'flex' }}>
-              {techItemList.map((title) => (
-                <li>
-                  <TechItemDiv>
-                    <div
-                      style={{
-                        position: 'relative',
-                        width: '2.8rem',
-                        height: '2.8rem',
-                      }}
-                    >
-                      <Image src="/react.svg" alt="react-icon" fill></Image>
-                    </div>
-
-                    <div style={{ marginRight: '1rem' }}>{title}</div>
-                  </TechItemDiv>
-                </li>
-              ))}
-            </ul>
+            {techStackMenu.map((item) => (
+              <TechStackMenuDiv
+                key={item.name}
+                checked={item.checked}
+                onClick={() => {
+                  filterTechStack(item.type);
+                  checkTechMenu(item.type);
+                }}
+              >
+                {item.name}
+              </TechStackMenuDiv>
+            ))}
+            <SearchDiv>
+              <FiSearch></FiSearch>
+              <SearchInput placeholder="Search anything"></SearchInput>
+            </SearchDiv>
           </div>
-          <SearchDiv>
-            <div
-              style={{
-                paddingRight: '10px',
-                marginRight: '10px',
-                borderRight: '1px solid',
-              }}
-            >
-              🔍
-            </div>
-            <SearchInput placeholder="Search anything"></SearchInput>
-          </SearchDiv>
+          <ul>
+            {techItemList.map((item) => (
+              <li
+                key={item.id}
+                onClick={() => {
+                  checkTechStack(item.id);
+                }}
+              >
+                <TechItemDiv checked={item.checked}>
+                  <div>
+                    <Image src={item.img} alt={item.name} fill></Image>
+                  </div>
+                  <div>{item.name}</div>
+                </TechItemDiv>
+              </li>
+            ))}
+          </ul>
         </section>
 
         <BoardCardList boardCards={boardCards}></BoardCardList>
         <Pagination></Pagination>
-      </section>
+      </MainDiv>
     </Div>
   );
 }
 
 const Div = styled.div`
-  font-family: var(--Noto-B);
   color: ${(props) => props.theme.main_brown};
   display: flex;
   flex-direction: column;
-  max-width: 1440px;
   margin: 0 auto;
+`;
+
+const MainDiv = styled.div`
+  /* padding: 0 11rem; */
+  width: 100%;
+  max-width: 1280px;
+
+  margin: 0 auto;
+  margin-top: 4.5rem;
+  & > section {
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 8rem;
+    & > div {
+      display: flex;
+      margin-bottom: 1rem;
+      & > div {
+        flex-shrink: 0;
+      }
+    }
+    & > ul {
+      display: flex;
+      flex-wrap: wrap;
+    }
+  }
 `;
 
 const SearchDiv = styled.div`
   display: flex;
-  width: 37.8rem;
+
+  flex-shrink: 1;
+  width: 40rem;
   height: 5.2rem;
   padding: 1.4rem;
   border: 1px solid;
+  margin-left: auto;
   border-color: ${(props) => props.theme.horizon_gray};
   border-radius: 6px;
+
+  & > svg {
+    padding-right: 10px;
+    margin-right: 10px;
+    border-right: 1px solid;
+    width: 2rem;
+    height: 2rem;
+  }
 `;
 
 const SearchInput = styled.input`
@@ -125,26 +156,40 @@ const SearchInput = styled.input`
   outline-style: none;
 `;
 
-const TechStackMenuDiv = styled.div`
+const TechStackMenuDiv = styled.div<{ checked: boolean }>`
   text-align: center;
-  width: 12rem;
-  padding: 2rem;
+  width: auto;
+  padding: 0 2rem 2rem 2rem;
   color: ${(props) => props.theme.unselected_text};
   border-bottom: 2px solid black;
   cursor: pointer;
-  &:hover {
-    border-bottom: 2px solid ${(props) => props.theme.sub_yellow};
-    color: black;
-  }
+  font-size: 2.4rem;
+  border-bottom: 2px solid
+    ${(props) => (props.checked ? props.theme.sub_yellow : '')};
+  color: ${(props) => (props.checked ? 'black' : '')};
 `;
 
-const TechItemDiv = styled.div`
+const TechItemDiv = styled.div<{ checked: boolean }>`
   display: flex;
-  width: fit-content;
-  margin: 1rem;
-  padding: 0.5rem;
-  justify-content: center;
   align-items: center;
+  justify-content: center;
+  margin: 1rem;
+  padding: 1.2rem 2rem 1.2rem 1.5rem;
   border: 1px solid;
+  box-shadow: ${(props) => (props.checked ? '0 0 0 3px inset' : '')};
   border-radius: 50px;
+  cursor: pointer;
+  &:hover {
+    transform: scale(1.1);
+  }
+
+  & > div:first-child {
+    position: relative;
+    width: 3.7rem;
+    height: 3.7rem;
+  }
+
+  & > div:last-child {
+    margin-right: 1rem;
+  }
 `;
