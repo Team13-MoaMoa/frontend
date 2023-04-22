bb48903b807f35aad2323bcabff3f117928655e4
jonghun <minn4072@gmail.com>
2023-04-22 18:27:51 +0900

    feat: key값 추가

--- src/components/common/BoardCardList/index.tsx
+++ src/components/common/BoardCardList/index.tsx
@@ -5,17 +5,27 @@ import BoardCardItem from '../BoardCardItem';
 function BoardCardList({ boardCards }: { boardCards: any }) {
   return (
     <Div>
-      {boardCards?.map(() => (
-        <BoardCardItem></BoardCardItem>
+      {boardCards?.map((_: any, i: any) => (
+        <BoardCardItem key={i}></BoardCardItem>
       ))}
     </Div>
   );
 }
 
 const Div = styled.div`
-  display: flex;
-  justify-content: space-evenly;
-  flex-wrap: wrap;
+  width: 100%;
+  display: grid;
+  place-items: center;
+
+  grid-template-columns: repeat(auto-fill, minmax(37.8rem, auto));
+  /* ${(props) => props.theme.mq.tablet} {
+    grid-template-columns: repeat(2, 1fr);
+  } */
+  ${(props) => props.theme.mq.mobile} {
+    grid-template-columns: repeat(1, 1fr);
+  }
+
+  gap: 3rem;
 `;
 
 export default BoardCardList;
