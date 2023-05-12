813c5b5fa52630b1d656fff88f8c98ac31e8bc2d
wisdomyeon <duswl8936@naver.com>
2023-05-12 19:12:34 +0900

    feat :  백그라운드 생성 및 위치조정

---
+++ src/components/ProfilerImg/index.tsx
@@ -0,0 +1,14 @@
+import styled from '@emotion/styled';
+
+function ProfilerImg() {
+  return <Div></Div>;
+}
+
+export default ProfilerImg;
+
+const Div = styled.div`
+  color: ${(props) => props.theme.main_brown};
+  display: flex;
+  justify-content: center;
+  margin: 12rem 0;
+`;
---
+++ src/pages/mypage/index.tsx
@@ -0,0 +1,38 @@
+import styled from '@emotion/styled';
+import ProfilerImg from '@/components/ProfilerImg';
+
+function MyPage() {
+  return (
+    <Div>
+      <BackgroundDiv></BackgroundDiv>
+      <InfoBackgroundDiv>
+        <ProfilerImg />
+      </InfoBackgroundDiv>
+    </Div>
+  );
+}
+
+export default MyPage;
+
+const Div = styled.div`
+  color: ${(props) => props.theme.main_brown};
+  height: 100vh;
+  margin: 0 0 12rem 0;
+`;
+
+const BackgroundDiv = styled.div`
+  height: 31rem;
+  width: 100%;
+  position: relative;
+  background-color: ${(props) => props.theme.main_brown};
+`;
+
+const InfoBackgroundDiv = styled.div`
+  height: 98rem;
+  width: 119rem;
+  border-radius: 4rem;
+  position: absolute;
+  top: 13%;
+  left: 18%;
+  background-color: #fff9fa;
+`;
