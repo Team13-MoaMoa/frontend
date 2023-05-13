ab9edf246e5e4be39379bf020e0f504414633a0c
jonghun <minn4072@gmail.com>
2023-05-14 04:55:46 +0900

    feat : type 선언

--- src/types/board/index.ts
+++ src/types/board/index.ts
@@ -1,16 +1,48 @@
-export type BoardCard = {
+export type BoardListResponseType = {
+  code: string;
+  message: string;
+  data: BoardListDataType;
+};
+
+export type BoardListDataType = {
+  content: BoardDataType[];
+  pageable: string;
+  last: boolean;
+  totalPages: number;
+  totalElements: number;
+  first: boolean;
+  size: number;
+  number: number;
+  sort: SortType;
+  numberOfElements: number;
+  empty: boolean;
+};
+
+export type BoardDataType = {
   id: number;
   title: string;
-  projectName: string;
+  project_name: string;
   content: string;
   deadline: number[];
   headcount: number;
-  jobPosition: string;
-  user: {
-    id: number;
-    nickname: string;
-    imageUrl: string;
-  };
-  techStackList: number[];
-  commentCount: number;
+  job_position: string;
+  user: UserType;
+  tech_stack_list: TechStackListType[];
+  comment_count: number;
+};
+
+export type TechStackListType = {
+  id: number;
+};
+
+export type UserType = {
+  id: number;
+  nickname: string;
+  image_url: null;
+};
+
+export type SortType = {
+  empty: boolean;
+  sorted: boolean;
+  unsorted: boolean;
 };
