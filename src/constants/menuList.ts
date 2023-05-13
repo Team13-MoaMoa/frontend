3dbe211f5db8df484fcbf2e2282487240d51fb6e
jonghun <minn4072@gmail.com>
2023-05-14 04:55:05 +0900

    feat : 상수 데이터 생성

---
+++ src/constants/menuList.ts
@@ -0,0 +1,6 @@
+export const techStackMenu = [
+  { name: '프론트엔드', type: 'FRONTEND' },
+  { name: '백엔드', type: 'BACKEND' },
+  { name: '기타', type: 'ETC' },
+  { name: '모두보기', type: 'ALL' },
+];
--- src/constants/techStackData.ts
+++ src/constants/techStackData.ts
@@ -1,43 +1,83 @@
-type techStackDataType = {
-  name: string;
-  id: number;
-  img: string;
-  type: string;
-};
+import ReactIcon from '/src/assets/techIcons/React.png';
+import HtmlIcon from '/src/assets/techIcons/Html.png';
+import NodeJsIcon from '/src/assets/techIcons/NodeJs.png';
+import PhpIcon from '/src/assets/techIcons/Php.png';
+import FigmaIcon from '/src/assets/techIcons/Figma.png';
+import DockerIcon from '/src/assets/techIcons/Docker.png';
+import JavaScriptIcon from '/src/assets/techIcons/JavaScript.png';
+import PythonIcon from '/src/assets/techIcons/Python.png';
+import GitIcon from '/src/assets/techIcons/Git.png';
+import GatsbyIcon from '/src/assets/techIcons/Gatsby.png';
 
 const techStackData = [
-  { name: 'React', id: 1, img: '/React.png', type: 'frontend', checked: false },
-  { name: 'Html', id: 2, img: '/Html.png', type: 'frontend', checked: false },
+  {
+    name: 'React',
+    id: 1,
+    img: ReactIcon,
+    type: 'FRONTEND',
+    checked: false,
+  },
+  {
+    name: 'Html',
+    id: 2,
+    img: HtmlIcon,
+    type: 'FRONTEND',
+    checked: false,
+  },
   {
     name: 'NodeJs',
     id: 3,
-    img: '/NodeJs.png',
-    type: 'backend',
+    img: NodeJsIcon,
+    type: 'BACKEND',
     checked: false,
   },
-  { name: 'Php', id: 4, img: '/Php.png', type: 'backend', checked: false },
-  { name: 'Figma', id: 5, img: '/Figma.png', type: 'etc', checked: false },
-  { name: 'Docker', id: 6, img: '/Docker.png', type: 'etc', checked: false },
+  {
+    name: 'Php',
+    id: 4,
+    img: PhpIcon,
+    type: 'BACKEND',
+    checked: false,
+  },
+  {
+    name: 'Figma',
+    id: 5,
+    img: FigmaIcon,
+    type: 'ETC',
+    checked: false,
+  },
+  {
+    name: 'Docker',
+    id: 6,
+    img: DockerIcon,
+    type: 'ETC',
+    checked: false,
+  },
   {
     name: 'JavaScript',
     id: 7,
-    img: '/JavaScript.png',
-    type: 'frontend',
+    img: JavaScriptIcon,
+    type: 'FRONTEND',
     checked: false,
   },
   {
     name: 'Python',
     id: 8,
-    img: '/Python.png',
-    type: 'backend',
+    img: PythonIcon,
+    type: 'BACKEND',
     checked: false,
   },
-  { name: 'Git', id: 9, img: '/Git.png', type: 'etc', checked: false },
+  {
+    name: 'Git',
+    id: 9,
+    img: GitIcon,
+    type: 'ETC',
+    checked: false,
+  },
   {
     name: 'Gatsby',
     id: 10,
-    img: '/Gatsby.png',
-    type: 'frontend',
+    img: GatsbyIcon,
+    type: 'FRONTEND',
     checked: false,
   },
 ];
