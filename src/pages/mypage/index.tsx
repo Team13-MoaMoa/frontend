cd3974fc041e046d69b63d78f57d950ec0bd25ee
seunghwan <sun1238799@naver.com>
2023-10-31 12:16:01 +0900

    refactor: 마이페이지 html구조 수정

--- src/pages/mypage/index.tsx
+++ src/pages/mypage/index.tsx
@@ -1,17 +1,27 @@
 import styled from '@emotion/styled';
-import Profile from '@/components/Profile';
-import EditProfile from '@/components/EditProfile';
 
-import CompleteProfile from '@/components/CompleteProfile';
 import ProfileZone from '@/components/ProfileZone';
+import { useEffect, useState } from 'react';
+import { authInstance } from '@/api/axiosCustom';
 
 function MyPage() {
+  const [data, setData] = useState<any>();
+  useEffect(() => {
+    (async () => {
+      const res = await authInstance('/users/projects?page=1');
+      setData(res.data.data);
+      console.log(res.data);
+    })();
+  }, []);
   return (
     <Div>
-      <BackgroundDiv></BackgroundDiv>
-      <InfoBackgroundDiv>
-        <ProfileZone />
-      </InfoBackgroundDiv>
+      <InfoBackgroundSection>
+        <BackgroundDiv />
+        <InfoUserDiv>
+          <ProfileZone />
+        </InfoUserDiv>
+      </InfoBackgroundSection>
+      hi
     </Div>
   );
 }
@@ -21,7 +31,12 @@ export default MyPage;
 const Div = styled.div`
   color: ${(props) => props.theme.main_brown};
   height: 100vh;
-  margin: 0 0 12rem 0;
+  margin: 0 0 15rem 0;
+`;
+
+const InfoBackgroundSection = styled.section`
+  width: 100vw;
+  height: 65rem;
 `;
 
 const BackgroundDiv = styled.div`
@@ -31,13 +46,13 @@ const BackgroundDiv = styled.div`
   background-color: ${(props) => props.theme.main_brown};
 `;
 
-const InfoBackgroundDiv = styled.div`
+const InfoUserDiv = styled.div`
   height: 98rem;
   width: 119rem;
   border-radius: 4rem;
   position: absolute;
   top: 13%;
-  left: 18%;
+  left: calc(50% - 59.5rem);
   display: flex;
   flex-wrap: wrap;
   background-color: #ffffff;
