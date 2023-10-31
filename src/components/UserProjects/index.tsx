b4d0806fba797da0826d59190b81bf81c0e9dd83
seunghwan <sun1238799@naver.com>
2023-10-31 13:55:32 +0900

    feat: 찜한 데이터 , 글 작성 데이터 분류

--- src/components/UserProjects/index.tsx
+++ src/components/UserProjects/index.tsx
@@ -2,28 +2,43 @@ import React from 'react';
 import BoardCardList from '../common/BoardCardList';
 import { useEffect, useState } from 'react';
 import { authInstance } from '@/api/axiosCustom';
-import { BoardListType, BoardType } from '@/types/board';
+import { BoardListType } from '@/types/board';
 import styled from '@emotion/styled';
 import SelectBar from '../SelectBar';
 import { select } from '@/types/mypage';
 
 const UserProjects = () => {
   const [selectButton, setselectButton] = useState<select>('SAVE');
-  const [data, setData] = useState<BoardListType>();
+  const [saveList, setSaveList] = useState<BoardListType>();
+  const [writeList, setWriteList] = useState<BoardListType>();
+
   useEffect(() => {
     (async () => {
-      const res = await authInstance('/users/projects?page=1');
-      setData(res.data.data);
-      console.log(res.data.data.content);
+      try {
+        if (selectButton === 'SAVE') {
+          const res = await authInstance('/users/likes?page=1');
+          setSaveList(res.data.data);
+        } else if (selectButton === 'WRITE') {
+          const res = await authInstance('/users/projects?page=1');
+          setWriteList(res.data.data);
+        }
+      } catch (e) {
+        console.log(e);
+      }
     })();
-  }, []);
+  }, [selectButton]);
+
   return (
     <UserProjectsBox>
       <SelectBar
         setselectButton={setselectButton}
         selectButton={selectButton}
       />
-      <BoardCardList boardCards={data?.content} />
+      {selectButton === 'SAVE' ? (
+        <BoardCardList boardCards={saveList?.content} />
+      ) : (
+        <BoardCardList boardCards={writeList?.content} />
+      )}
     </UserProjectsBox>
   );
 };
