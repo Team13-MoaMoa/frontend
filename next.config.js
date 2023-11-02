467a0628593a1fdecc7e8a130ad63153d5880449
jonghun <minn4072@gmail.com>
2023-11-02 21:43:29 +0900

    chore: s3 주소 이미지 설정 next.config.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: false,
   images: {
     // FIXME: 우리 서비스의 S3주소로 대체하기
-    domains: ['image.idus.com'],
+    domains: ['image.idus.com', 'moamoa-s3.s3.ap-northeast-2.amazonaws.com'],
     formats: ['image/avif', 'image/webp'],
   },
 };
